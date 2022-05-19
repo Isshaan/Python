341541122ebe0092ddd8208e3e5cefd22079c6b6
Isshaan <105578113+Isshaan@users.noreply.github.com>
2022-05-19 23:14:07 +0530

    Python_2_Converting_US_Floors.py

---
+++ Python_2.py
@@ -0,0 +1,4 @@
+#Convert Elevator Floors
+Floor_Number_Abroad=input('What is the Floor Number in Europe/Asia/Africa?')
+Floor_Number_in_US=int(Floor_Number_Abroad)+1
+print(str(Floor_Number_Abroad)+'th Floor in the World is', str(Floor_Number_in_US)+'th Floor in US.')
