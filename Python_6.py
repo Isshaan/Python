c0e0aebad7ed889b7090ef4c914b5e578b05f773
Isshaan <105578113+Isshaan@users.noreply.github.com>
2022-05-26 09:56:10 +0530

    Create Python_6.py

---
+++ Python_6.py
@@ -0,0 +1,11 @@
+def Greet(Language) :
+    if Language=='es':
+        print('Hola')
+    elif Language=='fr':
+        print('Bonjour')
+    else:
+        print('Hello')
+
+Greet('en')
+Greet('fr')
+Greet('es')
