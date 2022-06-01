bcbf007870fa5aa5fb675cd8620bef05e3c4dcfc
Isshaan <105578113+Isshaan@users.noreply.github.com>
2022-06-01 23:52:05 +0530

    Update Python_11.py

--- Python_11.py
+++ Python_11.py
@@ -1,8 +1,18 @@
-List=[1,2,3,4,60,6,7,8,9,10]
+x=int(input('Enter the number of Values in the List\n'))
+if x<0:
+    print('Please Enter a Positive Integer')
+List=[]
+i=0
+while x>0 :
+    print('Please Enter value number', i+1)
+    value=int(input())
+    List.append(value)
+    i=i+1
+    x=x-1
 y=List[0]
-for x in List:
-    if y>x:
-      continue
+for j in List:
+    if y>j:
+        continue
     else:
-      y=x
-print('The greatest number in the List is', y)
+        y=j
+print('The greatest number from your entered list is', y)
